22342fd48beeed406e21d9299ae282c353597824
Shahmeer1999khan <shahmeer1999khan@gmail.com>
2023-08-07 11:32:21 +0500

    xpath

--- new.py
+++ new.py
@@ -9,7 +9,6 @@ from selenium.webdriver.chrome.service import Service
 import pandas as pd
 import undetected_chromedriver as uc
 from selenium.common.exceptions import NoSuchElementException
-import requests
 
 driver = webdriver.Chrome()
 
@@ -35,21 +34,21 @@ login= driver.find_element(by='xpath', value='//div//button[@class="_acan _acap 
 time.sleep(10)
 
 
-# def single_person():
-#     for searches in search1:
-#         driver.get(f"https://www.instagram.com/{searches}/")
-#         time.sleep(5)
-#         try:
-#             send_message_button = driver.find_element(by='xpath', value='//div[contains(text(), "Message")]')
-#             send_message_button.click()        
-#             time.sleep(10)
-#             message_input= driver.find_element(by='xpath',value='//p[@class="xat24cr xdj266r"]')
-#             message_input.send_keys("hello")
-#             message_input.send_keys(Keys.ENTER)
-#         except:
-#             pass
+def single_person():
+    for searches in search1:
+        driver.get(f"https://www.instagram.com/{searches}/")
+        time.sleep(5)
+        try:
+            send_message_button = driver.find_element(by='xpath', value='//div[contains(text(), "Message")]')
+            send_message_button.click()        
+            time.sleep(10)
+            message_input= driver.find_element(by='xpath',value='//p[@class="xat24cr xdj266r"]')
+            message_input.send_keys("hello")
+            message_input.send_keys(Keys.ENTER)
+        except:
+            pass
         
-# single_person()
+single_person()
 
 def send_messages_to_top_30_users():
     try:
@@ -63,39 +62,25 @@ def send_messages_to_top_30_users():
             time.sleep(15)
     except:
         pass
-    likes_button = driver.find_element(by='xpath',value='//span[@class="x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"]')
-    likes_button.click()
-    time.sleep(5)
 
-    likes1 = driver.find_element(by='xpath', value='//div[@style="height: 356px; overflow: hidden auto;"]')
-
-    # Desired number of links to collect
-    target_links_count = 30
-    action =ActionChains(driver)
-    
-
-    while True:
-        # Scroll within the likes1 element using JavaScript
-        scroll_script = "arguments[0].scrollBy(0, arguments[0].scrollHeight);"
-        # driver.execute_script(scroll_script, likes1)
-        # action.send_keys(likes1,Keys.DOWN)
-        action.move_to_element(likes1).send_keys(Keys.DOWN).perform()
-        
-        time.sleep(2)  # You can adjust this delay as needed
-        
-        likes_popup = likes1.find_elements(by='xpath', value='.//a')
-        collected_links = [link.get_attribute("href") for link in likes_popup]
-        unique_links = list(set(collected_links))
-        
-        if len(unique_links) >= target_links_count:
-            break
-
-    for link in unique_links:
-        print(link)
-
-    for link in unique_links:
+    for link in links:
         driver.get(link)
         time.sleep(5)
+        likes_button = driver.find_element(by='xpath', value='//span[@class="x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"]')
+        likes_button.click()
+        time.sleep(5)
+        temp = []
+        likes_popup = driver.find_elements(by='xpath', value='//div[@class="x1dm5mii x16mil14 xiojian x1yutycm x1lliihq x193iq5w xh8yej3"]//a[@class="x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk x78zum5 xdl72j9 xdt5ytf x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt xnz67gz x14yjl9h xudhj91 x18nykt9 xww2gxu x9f619 x1lliihq x2lah0s x6ikm8r x10wlt62 x1n2onr6 x1ykvv32 xougopr x159fomc xnp5s1o x194ut8o x1vzenxt xd7ygy7 xt298gk x1xrz1ek x1s928wv x1n449xj x2q1x1w x1j6awrg x162n7g1 x1m1drc7 x1ypdohk x4gyw5p _a6hd"]')
+        for i in range(min(60, len(likes_popup))):
+            link = likes_popup[i].get_attribute("href")
+            temp.append(link)
+            if len(set(temp)) >= 30:
+                break
+        for link in temp:
+            print(link)
+            driver.get(link)
+            time.sleep(5)
+
         
         try:
             send_message_button = driver.find_element(by='xpath', value='//div[contains(text(), "Message")]')
